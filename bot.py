7554b48813413920ecf07615224b365479a2e0b5
Dat Le <datle.it@gmail.com>
2024-09-04 17:41:14 +0700

    Refactor and support multi-group

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 from datetime import datetime, timedelta, time
 from telegram import Update
-from telegram.ext import  Updater, CommandHandler,CallbackContext,ApplicationBuilder,ContextTypes, ConversationHandler,MessageHandler, filters
+from telegram.ext import  CommandHandler, CallbackContext, ApplicationBuilder, ConversationHandler, MessageHandler, filters
 import sqlite3
 import os
 from dotenv import load_dotenv
@@ -20,7 +20,6 @@ conn = sqlite3.connect('mydatabase.db')
 cursor = conn.cursor()
 
 # Create a table if not exists
-
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
@@ -34,17 +33,23 @@ cursor.execute('''
         days TEXT,
         message TEXT,
         times TEXT,
-        status BOOL
+        status BOOL,
+        groups TEXT
     )
 ''')
 
 conn.commit()
 
 # Get user from db
-cursor.execute('SELECT * from users')
-users = cursor.fetchall()
-ALLOWED_USERNAMES = '\n'.join([f"{user[1]}" for user in users])
-#print(ALLOWED_USERNAMES)
+OWNER = 'datlq3'
+
+def is_owner(user):
+    return user == OWNER
+
+def is_authorized(user):
+    cursor.execute('SELECT * from users')
+    users = cursor.fetchall()
+    return user in users
 
 def user_exists(user_name):
     cursor.execute('SELECT * FROM users WHERE username = ?', (user_name,))
@@ -53,7 +58,7 @@ def user_exists(user_name):
     else: 
         return False
 
-def insert (user_name_list):
+def insert(user_name_list):
     existed_users = []
     for user_name in user_name_list: 
         if user_exists(user_name):
@@ -66,15 +71,15 @@ def insert (user_name_list):
     else:
         return True, None
     
-def remove (user_name_list):
+def remove(user_name_list):
     for user_name in user_name_list:
         cursor.execute('DELETE FROM users WHERE username = ?', (user_name,))
         conn.commit()
 
-async def remove_func(update,context):
+async def remove_member(update, context):
     username = update.message.from_user.username
-    if username in ALLOWED_USERNAMES:
-        user_name_list = context.args
+    if is_owner(username):
+        user_name_list = update.message.text
         if len(user_name_list) > 0:    
             remove(user_name_list)
             await update.message.reply_text(f'Xóa người dùng thành công')
@@ -83,10 +88,10 @@ async def remove_func(update,context):
     else:
         await update.message.reply_text("You are not authorized to use this bot.")
 
-async def insert_func (update, context):
-    username = update.message.from_user.username
-    if username in ALLOWED_USERNAMES:
-        user_name_list = context.args
+async def add_member(update, context):
+    username = update.message.from_user.username 
+    if is_owner(username):
+        user_name_list = update.message.text
         if len(user_name_list) > 0:
             res , existed_users = insert(user_name_list)
             if not res:
@@ -99,9 +104,9 @@ async def insert_func (update, context):
         # Notify unauthorized user
         await update.message.reply_text("You are not authorized to use this bot.")
 
-async def show(update,context):
+async def show_member(update, context):
     username = update.message.from_user.username
-    if username in ALLOWED_USERNAMES:
+    if is_owner(username):
         cursor.execute('SELECT * from users')
         users = cursor.fetchall()
 
@@ -113,11 +118,32 @@ async def show(update,context):
     else:
         # Notify unauthorized user
         await update.message.reply_text("You are not authorized to use this bot.")
-"6652724069:AAH5_kzdguWLCeSP1ncrEzs5x02D9Zt9xyw"
+    await update.message.reply_text("Bạn có muốn thực hiện thay đổi member ?\n1.Thêm mới\n2.Xóa bỏ\nq.Thoát")
+    return MODIFY_MEMBER
 
-async def shout(update,context):
+async def modify_member(update: Update, context: CallbackContext) -> int:
     username = update.message.from_user.username
-    if username in ALLOWED_USERNAMES:
+    if is_owner(username):
+        user_choice = update.message.text.lower()
+        if user_choice == '1':
+            await update.message.reply_text("Điền member mà bạn muốn thêm")
+            return ADD_MEMBER
+        if user_choice == '2':
+            await update.message.reply_text("Điền member mà bạn muốn xóa")
+            return REMOVE_MEMBER    
+        elif user_choice == 'q':
+            await update.message.reply_text("Kết thúc việc chỉnh sửa")
+            return ConversationHandler.END
+        else:
+            await update.message.reply_text("Lựa chọn không phù hợp. Hay điền lựa chọn phù hợp hoặc q để kết thúc.")
+        return ConversationHandler.END
+    else:
+        # Notify unauthorized user
+        await update.message.reply_text("You are not authorized to use this bot.")
+
+async def shout(update, context):
+    username = update.message.from_user.username
+    if is_authorized(username):
         cursor.execute('SELECT * from users')
         users = cursor.fetchall()
         user_list = ' '.join([f"@{user[1]}" for user in users])
@@ -172,8 +198,8 @@ async def show_job(update: Update, context: CallbackContext) -> int:
     jobs = cursor.fetchall()
     status = ['Tắt','Bật']
 
-    if jobs and len(jobs[0]) >= 5:
-        job_list = '\n'.join([f"ID: {job[0]}, Ngày: {job[1]}, Công việc: {job[2]}, Thời gian: {job[3]}, Trạng thái: {status[job[4]]}" for job in jobs])
+    if jobs and len(jobs[0]) >= 6:
+        job_list = '\n'.join([f"ID: {job[0]}| Ngày: {job[1]}| Công việc: {job[2]}| Thời gian: {job[3]}| Trạng thái: {status[job[4]]}| Nhóm: {job[5]}" for job in jobs])
         await update.message.reply_text(f"Danh sách nhắc việc:\n{job_list}")
     else:
         await update.message.reply_text("Danh sách rỗng")
@@ -183,26 +209,24 @@ async def show_job(update: Update, context: CallbackContext) -> int:
 
 async def modify_job(update: Update, context: CallbackContext) -> int:
     username = update.message.from_user.username
-    if username in ALLOWED_USERNAMES:
+    if is_owner(username):
         user_choice = update.message.text.lower()
         if user_choice == '1':
             await update.message.reply_text("Điền thông tin về công việc bạn muốn thêm")
             await update.message.reply_text("Nhập ngày (1-31 hoặc monday-sunday):")
             context.user_data['step'] = 'day'
             return ADD_JOB
-        if user_choice == '2':
+        elif user_choice == '2':
             await update.message.reply_text("Điền ID mà bạn muốn xóa")
             context.user_data['step'] = 'remove'
             return REMOVE_JOB
-        if user_choice == '3':
+        elif user_choice == '3':
             await update.message.reply_text("Điền ID mà bạn muốn tắt/bật")
             context.user_data['step'] = 'switch'
             return SWITCH_JOB
-            
         elif user_choice == 'q':
             await update.message.reply_text("Kết thúc việc chỉnh sửa")
             return ConversationHandler.END
-
         else:
             await update.message.reply_text("Lựa chọn không phù hợp. Hay điền lựa chọn phù hợp hoặc q để kết thúc.")
 
@@ -227,7 +251,7 @@ async def add_job(update: Update, context: CallbackContext) -> int:
             if 'days' not in context.user_data:
                 context.user_data['days'] = []
             context.user_data['days'].extend(days)
-            await update.message.reply_text(f"Nhập thông tin về công việc bạn muốn thêm hoặc nhập 'done' để kết thúc:")
+            await update.message.reply_text(f"Nhập thông tin về công việc bạn muốn thêm:")
             context.user_data['step'] = 'message'
         else:
             await update.message.reply_text("Ngày không hợp lệ. Nhập lại ngày (1-31 hoặc monday-sunday):")
@@ -238,8 +262,6 @@ async def add_job(update: Update, context: CallbackContext) -> int:
         context.user_data['message'] = message_input
         await update.message.reply_text(f"Nhập thời gian (HH:MM,HH:MM,...):")
         context.user_data['step'] = 'time'
-        # else:
-        #     await update.message.reply_text("Thông điệp không hợp lệ. Nhập lại thông điệp:")
 
     elif step == 'time':
         time_input = update.message.text
@@ -250,23 +272,29 @@ async def add_job(update: Update, context: CallbackContext) -> int:
             if 'times' not in context.user_data:
                 context.user_data['times'] = []
             context.user_data['times'].extend(times)
-            # await update.message.reply_text("Công việc đã được thêm thành công.")
-            context.user_data['step'] = 'done'  # Set the step to 'done'
-            # return ConversationHandler.END
+            context.user_data['step'] = 'group'
+            await update.message.reply_text(f"Nhập danh sách nhóm (CHAT_ID_1,CHAT_ID_2,...):")
         else:
             await update.message.reply_text("Thời gian không hợp lệ. Nhập lại thời gian (HH:MM,HH:MM,...):")
+    
+    elif step == 'group':
+        group_input = update.message.text
+        context.user_data['group'] = group_input
+        context.user_data['step'] = 'done'
 
     elif step == 'done':
         # Process the collected data and insert into the database
         days = context.user_data.get('days', [])
         message = context.user_data.get('message', '')
         times = context.user_data.get('times', [])
+        groups = context.user_data.get('groups', [])
 
         days_str = ','.join(days)
         times_str = ','.join(times)
+        groups_str = ','.join(groups)
         # print(f"{days},{message},{times}")
-        cursor.execute('INSERT INTO job (days, message, times, status) VALUES (?, ?, ?, ?)',
-                        (days_str, message, times_str, True))
+        cursor.execute('INSERT INTO job (days, message, times, status, groups) VALUES (?, ?, ?, ?, ?)',
+                        (days_str, message, times_str, True, groups_str))
         conn.commit()
 
         # Clear user_data for the next conversation
@@ -314,9 +342,9 @@ async def switch_job(update: Update, context: CallbackContext):
     return ConversationHandler.END
 
 async def help (update,context):
-    await update.message.reply_text("Bot Nhắc việc:\n/help: Thông tin các lệnh\n/show: Hiển thị danh sách thành viên\n/insert <tên các thành viên>: Thêm thành viên vào danh sách\n/remove <tên các thành viên>: Xóa các thành viên khỏi danh sách\n/show_job: Bắt đầu quá trình thêm công việc cần nhắc và hiện thị danh sách công việc")
+    await update.message.reply_text("Bot Nhắc việc:\n/help: Thông tin các lệnh\n/all: tag all member\n/show_member: hiển thị danh sách thành viên\n/show_job: hiển thị danh sách công việc")
 
-SHOW_JOB, MODIFY_JOB, ADD_JOB, REMOVE_JOB, SWITCH_JOB = range(5)
+MODIFY_MEMBER, ADD_MEMBER, REMOVE_MEMBER, MODIFY_JOB, ADD_JOB, REMOVE_JOB, SWITCH_JOB = range(7)
 
 class NotCommandFilter(filters.BaseFilter):
     def filter(self, message: Update) -> bool:
@@ -324,33 +352,37 @@ class NotCommandFilter(filters.BaseFilter):
 
 def main() -> None:
     application = ApplicationBuilder().token(TOKEN).build()
-     
-    insert_handler = CommandHandler("insert",insert_func)
-    show_handler = CommandHandler("show",show)
-    remove_handle = CommandHandler("remove",remove_func)
-    shout_handle = CommandHandler("all",shout)
+
+    shout_handler = CommandHandler("all",shout)
     help_hanlder = CommandHandler("help",help)
 
     not_command_filter = NotCommandFilter()
 
-    # Add the states to the conversation handler
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('show_job', show_job)],
+    member_handler = ConversationHandler(
+        entry_points=[CommandHandler('members', show_member)],
+        states={
+            MODIFY_MEMBER: [MessageHandler(not_command_filter, modify_member)],
+            ADD_MEMBER: [MessageHandler(not_command_filter, add_member)],
+            REMOVE_MEMBER: [MessageHandler(not_command_filter, remove_member)],
+        },
+        fallbacks=[CommandHandler('c', cancel)],
+    )
+
+    job_handler = ConversationHandler(
+        entry_points=[CommandHandler('jobs', show_job)],
         states={
             MODIFY_JOB: [MessageHandler(not_command_filter, modify_job)],
             ADD_JOB: [MessageHandler(not_command_filter, add_job)],
             REMOVE_JOB: [MessageHandler(not_command_filter, remove_job)],
             SWITCH_JOB: [MessageHandler(not_command_filter, switch_job)],
         },
-        fallbacks=[CommandHandler('cancel', cancel)],
+        fallbacks=[CommandHandler('c', cancel)],
     )
 
     application.add_handler(help_hanlder)
-    application.add_handler(insert_handler)
-    application.add_handler(show_handler)
-    application.add_handler(remove_handle)
-    application.add_handler(shout_handle)
-    application.add_handler(conv_handler)
+    application.add_handler(member_handler)
+    application.add_handler(job_handler)
+    application.add_handler(shout_handler)
     
     application.run_polling()
     
